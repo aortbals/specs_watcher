f688acd018bb738ca95da721f7b793bc1899bae9
Aaron Ortbals <aaron.ortbals@gmail.com>
2014-11-21 16:14:48 -0600

    update the cli with aliases for search, availability, update readme

--- README.md
+++ README.md
@@ -4,28 +4,46 @@ TODO: Write a gem description
 
 ## Installation
 
-Add this line to your application's Gemfile:
-
-```ruby
-gem 'specs_watcher'
+```shell
+git clone git@vault.aaronortbals.com:aortbals/specs_watcher.git
+cd specs_watcher
+gem build specs_watcher.gemspec && gem install specs_watcher
 ```
-
-And then execute:
-
-    $ bundle
-
-Or install it yourself as:
-
-    $ gem install specs_watcher
 
 ## Usage
 
-TODO: Write usage instructions here
+### Commands
 
-## Contributing
+Commands:
 
-1. Fork it ( https://github.com/[my-github-username]/specs_watcher/fork )
-2. Create your feature branch (`git checkout -b my-new-feature`)
-3. Commit your changes (`git commit -am 'Add some feature'`)
-4. Push to the branch (`git push origin my-new-feature`)
-5. Create a new Pull Request
+Check availability for an item by zip code and UPC
+
+```
+specs_watcher availability ZIP UPC
+```
+
+Search through Spec's Liquor Inventory
+
+```
+specs_watcher search [KEYWORD] [(--category | -c) CATEGORY] [(--verbose | -v)]
+```
+
+Describe available commands or one specific command
+
+```
+specs_watcher help [COMMAND]
+```
+
+#### Search
+
+Search by keyword
+
+```
+specs_watcher search balvenie
+```
+
+List a category
+
+```
+specs_watcher -c bourbon
+```
--- lib/specs_watcher/cli.rb
+++ lib/specs_watcher/cli.rb
@@ -3,15 +3,22 @@ require 'thor'
 module SpecsWatcher
   class CLI < Thor
 
-    desc "search --category=CATEGORY", "Search through Spec's Liquor Inventory"
-    long_desc <<-EOS
+    map 's' => 'search'
+    map 'a' => 'availability'
 
-      Categories:
-        armagnac, blended_whiskey, bourbon, boutique_bourbon, brandy, canadian_whiskey, cocktails, cognac, cream_liqueurs, decanters, eau_de_vie, flavored_vodka, gift_liquor, gin, grappa, imported_vodka, irish_whisky, liqueur, miniatures_50ml, other_whiskeys, rum, scotch_blends, scotch_malts, tequila, tequila_super_premium, us_vodka, whiskey_flavored
+    desc "search [KEYWORD] [(--category | -c)=CATEGORY] [(--verbose | -v)]", "Search through Spec's Liquor Inventory"
+    long_desc <<-EOS
+      Search through or list Spec's Liquor Inventory
+
+    Alias: s
+
+    Categories:
+
+      armagnac, blended_whiskey, bourbon, boutique_bourbon, brandy, canadian_whiskey, cocktails, cognac, cream_liqueurs, decanters, eau_de_vie, flavored_vodka, gift_liquor, gin, grappa, imported_vodka, irish_whisky, liqueur, miniatures_50ml, other_whiskeys, rum, scotch_blends, scotch_malts, tequila, tequila_super_premium, us_vodka, whiskey_flavored
 
     EOS
-    option :category, aliases: :c
-    option :verbose
+    option :category, aliases: '-c'
+    option :verbose, aliases: '-v'
     def search(keyword=nil)
       searcher_options = options.dup
       searcher_options.merge!({keyword: keyword}) if keyword
@@ -27,6 +34,11 @@ module SpecsWatcher
     end
 
     desc "availability ZIP UPC", "Check availability for an item by zip code and UPC"
+    long_desc <<-EOS
+      Check availability for an item by zip code and UPC
+
+    Alias: a
+    EOS
     def availability(zip, upc)
       results = Availability.search({ zip: zip, upc: upc })
       puts results[:title]
